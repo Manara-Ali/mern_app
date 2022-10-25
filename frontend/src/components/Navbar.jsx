8a815d4897b6ad862fafd0500b4ff11befa4b47a
Manara-Ali <manaraali64@gmail.com>
2022-10-25 19:07:51 -0400

    Login complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,13 +6,15 @@ import { Link } from "react-router-dom";
 
 // CREATE COMPONENT
 const Navbar = ({ user }) => {
-  // console.log(user);
+  console.log(user);
   return (
     <nav>
       <Link to="/orders">Order History</Link>
       &nbsp; | &nbsp;
       <Link to="/orders/new/:id">New Order</Link>
-      <span style={{ paddingLeft: "10px" }}>Welcome {user.newUser.name}</span>
+      <span style={{ paddingLeft: "10px" }}>
+        Welcome {user.newUser?.name || user.currentUser?.name}
+      </span>
     </nav>
   );
 };
